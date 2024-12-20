1104774c2942b3f011698db91990e50232421e49
Moinulilm10 <moinulilm10@gmail.com>
2024-12-21 01:48:37 +0600

    Updated login form header in admin/login.php

--- admin/login.php
+++ admin/login.php
@@ -18,7 +18,7 @@
             <div class="col-md-6">
                 <div class="card">
                     <h5 class="card-header">
-                        Login Form for login
+                        Login Form
                     </h5>
                     <div class="card-body">
                         <form>
