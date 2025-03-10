c8f00a401cc7865f63805d8add37999de06c9d77
Moinul Islam <moinulilm10@gmail.com>
2025-03-10 15:06:56 +0600

    `Removed unnecessary code and fixed minor formatting issues in admin/register.php`

--- admin/register.php
+++ admin/register.php
@@ -1,24 +1,22 @@
 <?php
 
 include_once '../classes/Register.php';
+
 $rs = new Register();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $addUser = $rs->addUser($_POST);
 }
+
 ?>
 
 <!doctype html>
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Sign Up</title>
 </head>
 
@@ -26,30 +24,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <div class="container py-5">
         <div class="row d-flex justify-content-center">
             <div class="col-md-6">
-                <span>
-                    <?php
-                    if (isset($addUser)) {
-                    ?>
-                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                            <?php $addUser; ?>
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                    <?php
-                    }
-                    ?>
-                </span>
+                <?php if (isset($addUser)) { ?>
+                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                        <?php echo $addUser; ?>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                    </div>
+                <?php } ?>
+
                 <div class="card">
-                    <h5 class="card-header">
-                        Sign Up
-                    </h5>
+                    <h5 class="card-header">Sign Up</h5>
                     <div class="card-body">
                         <form action="" method="POST">
-
                             <div class="mb-3">
                                 <label class="form-label">Username</label>
-                                <input type="name" name="username" class="form-control">
+                                <input type="text" name="username" class="form-control">
                             </div>
 
                             <div class="mb-3">
@@ -76,11 +64,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         </div>
     </div>
 
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
